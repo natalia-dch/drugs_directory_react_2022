747734e6decb72062457fba006ba1578e859be52
Aryslan Yakshibaev <yak.aryslan.1999@gmail.com>
2022-03-14 17:23:26 +0300

    Start add axios

--- src/pages_front/DrugsPage.tsx
+++ src/pages_front/DrugsPage.tsx
@@ -1,149 +1,133 @@
 // @ts-nocheck
-import React, {Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import './DrugsPage.css';
 import Button from '@mui/material/Button';
+import { useQuery } from 'react-query'
 import { InputAdornment, Input, Card, TextField } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import { useNavigate } from "react-router-dom";
+import axios from 'axios'
 import Brief from '../contracts'
 
-
-interface IProps {
-}
-
-interface IState {
-  drugs?: Brief[]; //id, name
-}
-
 const myDrugs = [
-{ "id": 0, "name": "изониазид"},
-{ "id": 1, "name": "рифампицин"},
-{ "id": 2, "name": "рифабутин"},
-{ "id": 3, "name": "рифапентин"},
-{ "id": 4, "name": "пиразинамид"},
-{ "id": 5, "name": "этамбутол"},
-{ "id": 6, "name": "изониазид"},
-    { "id": 7, "name": "рифампицин"},
-      { "id": 8, "name": "рифабутин"},
-      { "id": 9, "name": "рифапентин"},
-      { "id": 10, "name": "пиразинамид"},
-      { "id": 11, "name": "этамбутол"}
 ];
 
 function BriefCard(props) {
     const navigate = useNavigate();
-    return(
-      <div key={props.item.id} className="bigCard">
-      <div className="drugCard" onClick={() => {
-        props.handleClick(props.item.id);
-        if(props.item.id!=-1){
-          if(props.isDrugPage){
-            navigate("/drug");
-          }
-    }
-  }} >
-      <p className={props.isBold?"boldName notOverFl":"notOverFl"}>{props.item.name}</p>
-      </div>
-      <hr/>
-      </div>
+    return (
+        <div className="bigCard">
+            <div className="drugCard" onClick={() => {
+                props.handleClick(props.item.id);
+                if (props.item.id != -1) {
+                    if (props.isDrugPage) {
+                        navigate("/drug");
+                    }
+                }
+            }} >
+                <p className={props.isBold ? "boldName notOverFl" : "notOverFl"}>{props.item.name}</p>
+            </div>
+            <hr />
+        </div>
     )
 }
 
-class Drugs extends Component<IProps, IState> {
-  constructor(props : IProps) {
-  super(props);
-    this.state = {
-      drugs: [],
-      input: "",
+/* class Drugs extends Component<IProps, IState> {
+*     constructor(props: IProps) {
+*         super(props);
+*         this.state = {
+*             drugs: [],
+*             input: "",
+*         }
+*         this.handleSearch = this.handleSearch.bind(this);
+*         this.handleDrugClick = this.handleDrugClick.bind(this);
+*     }
+*
+*     componentDidMount() {
+*         this.handleSearch("");
+*     }
+*
+*     handleDrugClick(id) {
+*         console.log("id" + id + "was clicked");
+*         if (id == -1) {
+*             this.setState({
+*                 input: "",
+*             })
+*             this.handleSearch("");
+*         }
+*         else {
+*             this.props.handleClick(id);
+*         }
+*     }
+*
+*
+*     render() {
+*     }
+* } */
+
+interface IProps {
+    isDrugPage?: boolean;
+}
+
+interface IState {
+    drugs?: Brief[]; //id, name
+}
+
+
+const DrugsPure = (props: IProps) => {
+    const [search, setSearch] = useState("");
+    const [realSearch, setRealSearch] = useState("");
+
+    const { data, isLoading, isError
+    } = useQuery(["brief", realSearch], () => axios.get(`http://localhost:8080/api/drugs/brief?search=${realSearch}`));
+
+    const handleSearch = (value) => {
+        setRealSearch(value)
     }
-    this.handleSearch = this.handleSearch.bind(this);
-    this.handleDrugClick = this.handleDrugClick.bind(this);
-  }
 
-componentDidMount() {
-  this.handleSearch("");
-  }
-
-handleDrugClick(id) {
-console.log("id"+id+"was clicked");
-if(id==-1){
-this.setState({
-    input : "",
-  })
-this.handleSearch("");
-}
-else{
-  this.props.handleClick(id);
-}
-}
-
-handleSearch(value) {
-       // let value = this.state.input;
-       console.log(value)
-       let result = []
-       if(value=="")
-       //TODO ask for all drugs
-        {
-          this.props.handleClick(-1); //remove bold ???
-          console.log("asking for all drugs")
-          result=myDrugs
+    const handleDrugClick = (id) => {
+        console.log("id" + id + "was clicked");
+        if (id == -1) {
+            setSearch("");
+            handleSearch("");
         }
-      else
-      //TODO ask for specific ones
-      {
-        console.log("asking for specific drugs")
-        result = []
-      }
-       //get result
-       if(result.length==0){
-      this.props.handleClick(-1); //remove bold ???
-      console.log("got nothing");
-       this.setState({
-         drugs: [{ "id": -1, "name": "ничего не найдено.\n сбросить фильтрацию?"}],
-       })
-     }
-       else{
-      console.log("got"+result)
-      //change state
-      this.setState({
-        drugs: result,
-      });}
+        else {
+            props.handleClick(id);
+        }
     }
 
-  render(){
-    const elems = this.state.drugs.map((item)=>{
-      return(
-        <BriefCard isBold={!this.props.isDrugPage && this.props.sItem!=-1 && this.props.sItem==item.id }
-        item={item}
-        isDrugPage={this.props.isDrugPage}
-        handleClick={() => this.handleDrugClick(item.id)} />
-      )
-    })
-    return(<div className={this.props.isDrugPage?"wrapper":"wrapperSL"}>
-    <TextField className="drugInput"
-    onKeyPress={(e) => {
- if (e.key === 'Enter') {
-   this.handleSearch(e.target.value);
- }}}
-       id="input-with-icon-adornment"
-       value={this.state.input}
-       onChange={(e) =>
-         this.setState({
-           input : e.target.value,
-         })
-       }
-      label="поиск по названию" variant="standard"
-       InputProps={{
-   startAdornment: (
-     <InputAdornment position="start">
-                  <SearchIcon />
-     </InputAdornment>
-   ),
- }}/>
-       {elems}
-      </div>)
-  }
+    return (<div className={props.isDrugPage ? "wrapper" : "wrapperSL"}>
+        <TextField className="drugInput"
+            onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                    handleSearch(e.target.value);
+                }
+            }}
+            id="input-with-icon-adornment"
+            value={search}
+            onChange={(e) =>
+                setRealSearch(e.target.value)
+            }
+            label="поиск по названию" variant="standard"
+            InputProps={{
+                startAdornment: (
+                    <InputAdornment position="start">
+                        <SearchIcon />
+                    </InputAdornment>
+                ),
+            }} />
+        {isLoading ? "Loading..." :
+            isError ? "Error" :
+                data.data.length > 0 ? (data.data.map((item) => (
+                    <BriefCard key={item.id} isBold={props.isDrugPage && props.sItem != -1 && props.sItem == item.id}
+                        item={item}
+                        isDrugPage={props.isDrugPage}
+                        handleClick={() => handleDrugClick(item.id)} />
+                )
+                )
+                ) : "Ничего не нашлось. Сбросить фильтрацию?"
+        }
+    </div>)
 }
 
-export default Drugs;
+export default DrugsPure;
